d44ea913199e6e9abc297fa6ef4a18387b0082bb
Maoyu <3197206779@qq.com>
2024-11-13 19:50:40 +0800

    更新config.example文件

--- tool/recent/config.example.py
+++ tool/recent/config.example.py
@@ -3,13 +3,12 @@ CLIENT_NAME = 'Master_01'
 LOG_PATH = ''
 LOG_LEVEL = 'debug' # debug/info
 
-MYSQL_HOST=''
-MYSQL_PORT=3306
-MYSQL_USERNAME = ''
-MYSQL_PASSWORD = ''
+REGION_UTC_LIST = {1:8, 2:1, 3:-7, 4:3, 5:8}
 
 # master配置
-MASTER_DB_PATH = r''
+MASTER_DB_PATH = r'F:\temp\db'
+MASTER_API_URL = 'http://127.0.0.1:8000'
 
 # slave配置
 SALVE_REGION = [1,2,3,4,5]
+SALVE_API_URL = 'http://127.0.0.1:8000'
