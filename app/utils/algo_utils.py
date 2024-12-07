4aa498653d41c9f780998f737b4e55b3fd8cf5a2
Hanserprpr <2041283903@qq.com>
2024-12-07 23:32:15 +0800

    设置数据过期时间

--- app/utils/algo_utils.py
+++ app/utils/algo_utils.py
@@ -202,7 +202,9 @@ class Rating_Algorithm:
 
                 redis = RedisConnection.get_connection()
                 await redis.zadd(f"region:{region_id}:ship:{ship_id}", {user_id: personal_rating})
+                await redis.expire(f"region:{region_id}:ship:{ship_id}", 3600)
                 await redis.hset(f"ship_data:{ship_id}:{user_id}", mapping=user_data)
+                await redis.expire(f"ship_data:{ship_id}:{user_id}", 3600)
                 if not await redis.hexists(f"user_data:{user_id}", "username"):
                     user_data = {}
                     user_data = await RankDataModel.get_user(user_id, region_id)
@@ -230,7 +232,8 @@ class Rating_Algorithm:
                                 "clan_rank": league
                             }
                             print(user_data)
-                            await redis.hset(f"user_data:{user_id}", mapping=user_data)
+                            await redis.hset(f"user_data:{user_id}", mapping=user_data, expire=86400)
+                            await redis.expire(f"user_data:{user_id}", 3600)
                         else:
                             i = i - 1
                     else:
