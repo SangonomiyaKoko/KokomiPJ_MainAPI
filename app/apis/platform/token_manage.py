7ce0d7f64459275d905f7fa51c312e0a840277de
Maoyu <3197206779@qq.com>
2025-03-05 21:23:25 +0800

    实现将用户token同步到缓存的代码

--- app/apis/platform/token_manage.py
+++ app/apis/platform/token_manage.py
@@ -13,13 +13,13 @@ class GameUser:
             if token_result['code'] != 1000:
                 return token_result
             redis = RedisConnection.get_connection()
-            cache_key = f"token_cache:{token_type}:{region_id}:{account_id}"
-            token_cache = redis.get(cache_key)
+            cache_key = f"token_cache_{token_type}:{region_id}:{account_id}"
+            token_cache = await redis.get(cache_key)
             if token_cache:
                 if token_result['data']['token_value'] != token_result:
-                    redis.set(cache_key, token_result['data']['token_value'])
+                    await redis.set(cache_key, token_result['data']['token_value'])
             else:
-                redis.set(cache_key, token_result['data']['token_value'])
+                await redis.set(cache_key, token_result['data']['token_value'])
             return token_result
         except Exception as e:
             raise e
@@ -27,9 +27,20 @@ class GameUser:
             gc.collect()
 
     @ExceptionLogger.handle_program_exception_async
-    async def set_user_token(region_id: int, account_id: int, token_type: int) -> ResponseDict:
+    async def set_user_token(region_id: int, account_id: int, token_value: int, token_type: int) -> ResponseDict:
         try:
-            ...
+            token_result = await UserAccessToken.set_ac_value(account_id, region_id, token_value)
+            if token_result['code'] != 1000:
+                return token_result
+            redis = RedisConnection.get_connection()
+            cache_key = f"token_cache_{token_type}:{region_id}:{account_id}"
+            token_cache = await redis.get(cache_key)
+            if token_cache:
+                if token_result['data']['token_value'] != token_result:
+                    await redis.set(cache_key, token_result['data']['token_value'])
+            else:
+                await redis.set(cache_key, token_result['data']['token_value'])
+            return token_result
         except Exception as e:
             raise e
         finally:
@@ -38,7 +49,13 @@ class GameUser:
     @ExceptionLogger.handle_program_exception_async
     async def delete_user_token(region_id: int, account_id: int, token_type: int) -> ResponseDict:
         try:
-            ...
+            token_result = await UserAccessToken.delete_ac_value_by_id(account_id, region_id, token_type)
+            if token_result['code'] != 1000:
+                return token_result
+            redis = RedisConnection.get_connection()
+            cache_key = f"token_cache_{token_type}:{region_id}:{account_id}"
+            await redis.delete(cache_key)
+            return token_result
         except Exception as e:
             raise e
         finally:
